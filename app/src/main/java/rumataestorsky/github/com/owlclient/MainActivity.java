849742fad989a435239c012678e3e66a2d035708
Rumata Estorsky <rumata@sputnikchess.ru>
2016-06-07 19:48:41 +0300

    initial commit

---
+++ app/src/main/java/rumataestorsky/github/com/owlclient/MainActivity.java
@@ -0,0 +1,13 @@
+package rumataestorsky.github.com.owlclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
